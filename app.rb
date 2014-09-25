db7e6afa18fc4d349a8b08bb65a394072c5ef1dd
Brian McQueen <brian.d.mcqueen@gmail.com>
2014-09-25 10:31:40 -0400

    Added gallery creation

--- app.rb
+++ app.rb
@@ -33,6 +33,16 @@ get "/" do
   erb :home
 end
 
+get "/galleries/new" do
+  erb :new_gallery
+end
+
+post "/galleries" do
+  new_gallery_name = params[:gallery][:name]
+  Gallery.create(name: new_gallery_name)
+
+  redirect to("/")
+end
 get "/galleries/:id" do
   id = params[:id]
   @gallery = Gallery.find(id)
